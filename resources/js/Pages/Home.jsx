24bffb29bcecc9391f0f7354e3aec895ca719e18
zobirofkir <zobirofkir19@gmail.com>
2025-02-19 00:40:38 +0100

    add test direction

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,11 @@
+import { Link } from "@inertiajs/react"
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col gap-4 justify-center items-center h-screen">
         <h1 className="text-black font-bold text-center text-3xl">Welcome Home</h1>
+
+        <Link href="/login" className="text black font-bold text-4xl bg-black text-white px-4 py-2 rounded-md">Login</Link>
     </div>
   )
 }
