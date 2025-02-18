7b215d40510fb0f7d93d18998633025cb27df6e9
zobirofkir <zobirofkir19@gmail.com>
2025-02-19 00:37:31 +0100

    download inertia

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <h1 className="text-black font-bold text-center text-3xl">Welcome Home</h1>
+    </div>
+  )
+}
+
+export default Home
